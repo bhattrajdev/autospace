076ed3c909aaf23294baa033500b64cd30f114c0
bhattrajdev <devrajbhatt010@gmail.com>
2024-10-26 20:07:37 +0545

    Created Users module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,19 +5,21 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver:ApolloDriver,
-      introspection:true,
-      fieldResolverEnhancers:['guards'],
-      autoSchemaFile:join(process.cwd(),'src/schema.gql'),
-      buildSchemaOptions:{
-        numberScalarMode: 'integer'
+      driver: ApolloDriver,
+      introspection: true,
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
       },
-    })
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
